2e6d8db494a9a9363e9bb5848aa5ee86d1a2f8ce
emfo2405 <emfo2405@student.miun.se>
2025-05-15 08:48:42 +0200

    Sökfunktion skapad

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,22 +2,33 @@ import { Component } from '@angular/core';
 import { Info } from '../models/info';
 import { CourseService } from '../services/course.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 //Properties
 courseInfo: Info[] = [];
+searchedCourse: Info[] = [];
+searchedValue: string = ""
 
 constructor(private courseService: CourseService) {}
 
 ngOnInit() {
   this.courseService.getCourseInfo().subscribe((courseInfo) => {
     this.courseInfo = courseInfo;
+    this.searchedCourse = courseInfo;
   })
+}
+
+searchCourse(): void {
+  this.searchedCourse = this.courseInfo.filter((info) => {
+    return info.code.toLowerCase().includes(this.searchedValue.toLowerCase()) ||
+     info.coursename.toLowerCase().includes(this.searchedValue.toLowerCase())
+  });
 }
 }
